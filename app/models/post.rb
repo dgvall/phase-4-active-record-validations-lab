fceb0231b0e91b1de0c3dc5086dcd41cb464fbda
Diego Valles <dgvall134@gmail.com>
2023-02-24 15:34:49 -0700

    commit tree 77df580d40c4f6edceb537a5b53918bc09208a5b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  # validate :clickbait
+
+  # def clickbait
+  #   value = 0
+  #   phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+  #   phrases.each do |p|
+  #     if title.include?(p)
+  #       value = value + 1
+  #     end
+  #   end
+  #   if value == 0
+  #     errors.add(:title, "not clickbait-y!")
+  #   end
+  # end
 end
